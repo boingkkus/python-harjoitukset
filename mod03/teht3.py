163c3dd94e8fd907b2be27ddf486d1795ac5c5dc
Tiia Kanerva <kanerva.tiia@gmail.com>
2026-08-26 18:54:58 +0300

    teht3 tehty

---
+++ mod03/teht3.py
@@ -0,0 +1,7 @@
+kanta_str = input("Anna kanta:")
+korkeus_str = input("Anna korkeus:")
+kanta = float(kanta_str)
+korkeus = float(korkeus_str)
+piiri = kanta + kanta + korkeus + korkeus
+pinta_ala = kanta * korkeus
+print("piiri on: ",piiri),print("pinta-ala on: ",pinta_ala)
