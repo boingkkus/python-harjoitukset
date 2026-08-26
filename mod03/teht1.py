93d574574b5c1782269358f234dcfe7a79d693fd
Tiia Kanerva <kanerva.tiia@gmail.com>
2026-08-26 18:43:24 +0300

    teht2 tehty

--- mod03/teht1.py
+++ mod03/teht1.py
@@ -1,2 +1,2 @@
 nimi = input("Mikä on nimesi:")
-print("Hei, " + nimi + ".")
+print("Hei, " + nimi + "!")
---
+++ mod03/teht2.py
@@ -0,0 +1,6 @@
+import math
+säde_str = input("Anna säde:")
+säde = float(säde_str)
+pii = math.pi
+pinta_ala = pii * säde
+print("Pinta-ala: ",pinta_ala)
