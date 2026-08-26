e4db847e2dda62577a6e8ffea0348c9527baf556
Tiia Kanerva <kanerva.tiia@gmail.com>
2026-08-26 19:44:21 +0300

    teht4 ja 5 lisätty

---
+++ mod03/teht4.py
@@ -0,0 +1,10 @@
+eka_str = input("Anna kokonaisluku: ")
+toka_str = input("Anna kokonaisluku: ")
+kolmas_str = input("Anna kokonaisluku: ")
+eka = float(eka_str)
+toka = float(toka_str)
+kolmas = float(kolmas_str)
+summa = eka + toka + kolmas
+tulo = eka * toka * kolmas
+keskiarvo = (eka + toka + kolmas) / 3
+print("Summa: ",summa),print("Tulo: ",tulo),print("Keskiarvo: ",keskiarvo)
---
+++ mod03/teht5.py
@@ -0,0 +1,11 @@
+leiviskä_str = input("Anna leivisköjen määrä: ")
+naula_str = input("Anna naulojen määrä: ")
+luoti_str = input("Anna luotien määrä: ")
+leiviskä = float(leiviskä_str)
+naula = float(naula_str)
+luoti = float(luoti_str)
+naula = leiviskä * 20
+luoti = naula * 32
+gramma = luoti * 13.3
+kilogramma = gramma
+print("Massa nykymittojen mukaan: "),print(kilogramma," kilogrammaa"),print(gramma," grammaa"): {kilogramma:10.2},{gramma:10.2}
