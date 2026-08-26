062222fbffcd6289edd8cefcc4af3dad9d8fef82
Tiia Kanerva <kanerva.tiia@gmail.com>
2026-08-26 20:08:10 +0300

    teht6 tehty

---
+++ mod03/teht6.py
@@ -0,0 +1,4 @@
+import random
+numerolukko1 = random.randint(0,9),random.randint(0,9),random.randint(0,9)
+numerolukko2 = random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6)
+print(f"Kolminumeroinen koodi on: {numerolukko1}"),print(f"Nelinumeroinen koodi on: {numerolukko2}")
