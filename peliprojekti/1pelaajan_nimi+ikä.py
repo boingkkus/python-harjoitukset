156e8f653caeaab33fff8a923e7f6dff8aff040e
Tiia Kanerva <kanerva.tiia@gmail.com>
2026-09-10 01:38:38 +0300

    projekti 3

--- peliprojekti/1pelaajan_nimi+ikä.py
+++ peliprojekti/1pelaajan_nimi+ikä.py
@@ -9,21 +9,39 @@ else:
     print("Olet alaikäinen pelaamaan peliä.")
 
 
-komento = input("Anna komento; jyrsi, kaiva, heitä, nuku tai lopeta: ")
+komento = input("Anna komento; jyrsi, kaiva, heitä, nuku, katso pussukkaan tai lopeta: ")
 
 while komento != "lopeta":
     if komento == "MAYDAY":
         break
     print("Suoritan toiminnon: " + komento)
     if komento == "jyrsi":
-        print("Etsit huoneesta maukkaan näköisen laturin ja alat mautustelemaan sitä. Sähköinen heinä on harvinainen herkku.")
+        print("Etsit huoneesta maukkaan näköisen laturin ja alat mutustelemaan sitä. Sähköinen heinä on harvinainen herkku.")
     if komento == "kaiva":
         print("Änget itsesi sängyn alle kauimpaan nurkkaan ja alat kaivamaan lattiaa muristen. 'Tänään alakerran naapurit saavat tuntea vihani.'")
-    if komento == "heitä":
-        print("Näet irtotavaran lattialla ja päätät tänään olevan sen viimeinen päivä. Noukit sen nököhampaittesi väliin ja viskaiset sen niin kovaa kuin kykenet. 'Pois minun lattialtani!'")
     if komento == "nuku":
         print("Suuntaat sukkalaatikon viereen nokosille huoneen nurkkaan. Vielä ei ole aika aamulle.")
-    komento = input("Anna komento; jyrsi, kaiva, heitä, nuku tai lopeta: ")
+    if komento == "heitä":
+        print("Näet juomakupin lattialla ja päätät tänään olevan sen viimeinen päivä. Noukit sen nököhampaittesi väliin ja viskaiset sen niin kovaa kuin kykenet. 'Pois minun lattialtani!'")
+        vastaus = input("Lisätäänkö juomakuppi pussukkaan? y/n:")
+        if vastaus == "y":
+            def inventaario(tavarat):
+                return
+            pussukka = ["Porkkana", "Laturin pala", "Papana?"]
+            pussukka.append("juomakuppi")
+            inventaario(pussukka)
+    if komento == "katso pussukkaan":
+        def inventaario(tavarat):
+            print("Katsotaan mitä pussukasta löytyy: ")
+            for t in tavarat:
+                print("- " + t)
+            return
+        pussukka = ["Porkkana", "Laturin pala", "Papana?"]
+        inventaario(pussukka)
+        
+
+       
+    komento = input("Anna komento; jyrsi, kaiva, heitä, nuku, katso pussukkaan tai lopeta: ")
 else:
     print("Näkemiin, tervetuloa pelaamaan uudestaan!")
 print("Toiminnot lopetettu. ")
--- peliprojekti/readme.md
+++ peliprojekti/readme.md
@@ -1,2 +1,5 @@
 Pupen päivä
 Tiia Kanerva
+
+En saanut lisättyä käyttäjältä kysyttyä asiaa listamuuttujaan. 
+En osannut lisätä funktiota sujuvasti koodiin.
